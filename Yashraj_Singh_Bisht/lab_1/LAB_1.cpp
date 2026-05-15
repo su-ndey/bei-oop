1b3025e55cab350319a65c9e24f6da5854b455e6
yash01062001 <yash01062001@gmail.com>
2026-05-15 22:31:40 +0545

    Lab 1 submission ~ Yashraj Singh Bisht

---
+++ Yashraj_Singh_Bisht/lab_1/LAB_1.cpp
@@ -0,0 +1,17 @@
+// Lab task one area of rectangle
+
+#include <iostream>
+using namespace std;
+int main() {
+ float length, width, area;
+
+ cout << "Enter length of the rectangle: ";
+ cin >> length;
+ cout << "Enter width of the rectangle: ";
+ cin >> width;
+
+ area = length * width;
+
+ cout << "Area of the rectangle = " << area << endl;
+ return 0;
+}
