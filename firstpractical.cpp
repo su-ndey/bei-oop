d2bb782f19ab13054aca16cb49ad7baf0276bb86
Pratik Puri <puripratik05@gmail.com>
2026-05-15 15:03:05 +0545

    Added Pratik_Puri  OOP labs

--- firstpractical.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int length,breadth;
-    cout<<"Enter the length and breadth of the rectangle: ";
-    cin>>length>>breadth;
-    cout<<"The perimeter of rectangle is "<<2*(length+breadth);
-}
