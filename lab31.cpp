fb46d9865414ff93dd1ed25005d7ca8c3f51fd37
safalrajghimire <safalghimire758@gmail.com>
2026-06-03 09:42:44 +0545

    Lab 3 Safal Ghimire

---
+++ lab31.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+class Time
+{
+    int day, hour, minute, second;
+
+public:
+    Time(int d, int h, int m, int s)
+    {
+        day = d;
+        hour = h;
+        minute = m;
+        second = s;
+    }
+    void addTime(Time t1, Time t2)
+    {
+        second = t1.second + t2.second;
+        minute = t1.minute + t2.minute + second / 60;
+        second %= 60;
+        hour = t1.hour + t2.hour + minute / 60;
+        minute %= 60;
+        day = t1.day + t2.day + hour / 24;
+        hour %= 24;
+    }
+    void display()
+    {
+        cout << day << " Day "
+             << hour << " Hour "
+             << minute << " Minute "
+             << second << " Second\n";
+    }
+};
+int main()
+{
+    Time t1(2, 10, 50, 40);
+    Time t2(1, 15, 20, 35);
+    Time t3(0, 0, 0, 0);
+    t3.addTime(t1, t2);
+    cout << "Time 1: ";
+    t1.display();
+    cout << "Time 2: ";
+    t2.display();
+    cout << "Result: ";
+    t3.display();
+    return 0;
+}
